112fb3aba6ccb34f974bc01257aa48dbf766cf80
Rushikesh Sutar <rushikeshmsutar@gmail.com>
2026-08-11 02:34:45 +0530

    commit: Changing node type for cluster

--- aws-eks-terraform/main.tf
+++ aws-eks-terraform/main.tf
@@ -178,7 +178,7 @@ resource "aws_eks_node_group" "node-grp" {
   subnet_ids      = [data.aws_subnet.subnet-1.id, data.aws_subnet.subnet-2.id]
   capacity_type   = "ON_DEMAND"
   disk_size       = 20
-  instance_types  = ["t2.large"]
+  instance_types  = ["c7i-flex.large"]
 
   labels = {
     env = "dev"
